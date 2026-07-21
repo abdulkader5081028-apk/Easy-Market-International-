3c8c11f212393e719b9a81ae9e8625c36dfe3eb6
abdulkader5081028-apk <abdulkader5081028@gmail.com>
2026-07-21 18:25:46 +0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        alibaba: {
+          orange: '#FF6600',
+          dark: '#1A1A1A',
+          gray: '#F5F5F7'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
